a1a940ba2312cc1fe629123b83c1c63a39ef87e4
Colin <cpireyre@student.42.fr>
2022-03-23 00:52:55 +0100

    functional architecture with infinite lists 🥲

--- snake.py
+++ snake.py
@@ -1,144 +1,55 @@
 import curses
-from curses import wrapper
-from itertools import count, islice
+from collections import namedtuple
+from itertools import count
+from more_itertools import side_effect, consume
 
-from collections import deque
-import random
-from random import randint
-random.seed(1)
+UP, DOWN, LEFT, RIGHT = curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT
 
+Game = namedtuple("Game", ["snakex", "snakey"])
 
-def orientation(u, v):
-    uy, ux, vy, vx = u, v
-    return ((uy - vy) // abs(uy - vy), (ux - vx) // abs(ux - vx))
-orientations = {(1, 0): ">",
-        (-1, 0): "<",
-        (0, 1): "^",
-        (0, -1): "v",}
-def pos(key, y, x, H, W):
-    if key == curses.KEY_LEFT:
-        x = (x - 1) % (W - 1)
-    elif key == curses.KEY_RIGHT:
-        x = (x + 1) % (W - 1)
-    elif key == curses.KEY_DOWN:
-        y = (y + 1) % (H - 1)
-    elif key == curses.KEY_UP:
-        y = (y - 1) % (H - 1)
-    return (y, x)
+def move(G, INPUT): # assumes origin is top left which comes from curses, hmm
+    x = G.snakex + (INPUT == RIGHT) - (INPUT == LEFT)
+    y = G.snakey - (INPUT == UP) + (INPUT == DOWN)
+    return G._replace(snakex=x, snakey=y)
+    
+def play(G, INPUT): # could dispatch with a dict?
+    """Takes game state and INPUT, returns updated game state"""
+    if INPUT in {UP, DOWN, LEFT, RIGHT}:
+        G = move(G, INPUT)
+    return G
 
+def show(G): # attributes?
+    """Takes a game state and returns a ready-to-render list of tuples (x, y, symbol)"""
+    return [(G.snakex, G.snakey, "&")]
 
-def shortestPath(source, sink, H, W, S):
-    Q = deque([(source, [])])
-    seen = set(S)
+INPUT_TIMEOUT_MS = -1
 
-    def neighbors(y, x):
-        return {                (y, (x + 1) % W), (y, (x - 1) % W),
-                ((y + 1) % H, x), ((y - 1) % H, x),
-                }
-    while Q:
-        v, path = Q.popleft()
-        if v == sink:
-            return path
-        moves = neighbors(*v) - seen
-        Q.extend((move, path + [v]) for move in moves)
-        seen.update(moves)
-
-    return None
-        
-def main(stdscr):
-    stdscr.clear()
+def initscr(scr):
     curses.curs_set(0)
     curses.use_default_colors()
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, i, -1)
-    stdscr.timeout(40)
+    scr.timeout(INPUT_TIMEOUT_MS)
 
-    H, W = stdscr.getmaxyx()
-    y, x = H // 2, W // 2
+def draw(scr):
+    def handler(serialized):
+        scr.clear()
+        for x, y, symbol in serialized:
+            scr.addstr(y, x, symbol)
+    return handler
 
-    def draw(stdscr, game, H, W):
-        snake, snakeSize, apples, apple, path = game
-        stdscr.clear()
-        stdscr.addstr(0, W // 3, "BADGER'S SNAKE. Score: %d" % (snakeSize - 1,))
-        stdscr.addstr(*apple, "@", curses.A_BOLD | curses.color_pair(203))
-        if path:
-            for y, x in path:
-                stdscr.addstr(y % H, x % W, ".")
-        for y, x in snake:
-            if (y, x) != snake[-1]:
-                stdscr.addstr(y, x, "&", curses.color_pair(29))
-            else:
-                stdscr.addstr(y, x, "&", curses.A_BOLD | curses.color_pair(29))
-        stdscr.refresh()
+def transduce(xf, consumer, signal):
+    """Sort of like iterate but with a stream of secondary inputs..."""
+    while signal:
+        consumer = xf(consumer, next(signal))
+        yield consumer
 
-    input = (stdscr.getch() for _ in count())
-    snake = deque([(y, x)])
-    snakeSize = 1
-    apples = ((randint(2, H - 2), randint(2, W - 2)) for _ in range(100))
-    apple = next(apples)
-    path = shortestPath((y, x), apple, H - 1, W - 1, snake)
-    game = (snake, snakeSize, apples, apple, path)
+def main(scr):
+    initscr(scr)
+    G = Game(10, 10)
+    inputs = (scr.getch() for _ in count())
+    states = transduce(play, G, inputs)
+    frames = map(show, states)
+    consume(side_effect(draw(scr), frames))
 
-    def move(key, snake, snakeSize, apples, apple, path):
-        if key != -1:
-            coord = pos(key, *snake[-1], H, W)
-        else:
-            coord = path[1]
-        if snakeSize > 1 and coord == snake[-2]:
-            deltay = snake[-1][0] - coord[0]
-            deltax = snake[-1][1] - coord[1]
-            coord = ((snake[-1][0] + deltay) % H, (snake[-1][1] + deltax) % W)
-        if coord not in snake:
-            if coord == apple:
-                apple = next(apples, 0)
-                snakeSize += 1
-                if not apple:
-                    return snakeSize - 1
-            elif len(snake) >= snakeSize:
-                snake.popleft()
-            snake.append(coord)
-            path = shortestPath(coord, apple, H-1, W-1, snake)
-            return (snake, snakeSize, apples, apple, path)
-        else:
-            return snakeSize - 1
-
-    lastkey = None
-    draw(stdscr, game, H, W)
-    for key in input:
-        if key == -1 and lastkey == None:
-            continue
-        if key == -1:
-            key = lastkey
-        else:
-            lastkey = key
-        game = move(key, *game)
-        if type(game) == tuple:
-            draw(stdscr, game, H, W)
-        else:
-            curses.flash()
-            stdscr.addstr(H // 2, W // 2, "You win! Score: %d" % (game,))
-            stdscr.refresh()
-            curses.delay_output(3000)
-            return game
-score = wrapper(main)
-
-def seecolors(stdscr):
-    curses.start_color()
-    curses.use_default_colors()
-    for i in range(0, curses.COLORS):
-        curses.init_pair(i + 1, i, -1)
-    stdscr.addstr(0, 0, '{0} colors available'.format(curses.COLORS))
-    maxy, maxx = stdscr.getmaxyx()
-    maxx = maxx - maxx % 5
-    x = 0
-    y = 1
-    try:
-        for i in range(0, curses.COLORS):
-            stdscr.addstr(y, x, '{0:5}'.format(i), curses.color_pair(i))
-            x = (x + 5) % maxx
-            if x == 0:
-                y += 1
-    except curses.ERR:
-        pass
-    stdscr.getch()
-# wrapper(seecolors)
+core = curses.wrapper(main)
