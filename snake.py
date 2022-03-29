938f459db163409462392b3cefbffd04c5bbdd2b
Colin <cpireyre@student.42.fr>
2022-03-30 00:35:03 +0200

    Fixed camera mode lol

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 import curses
 from collections import namedtuple
-from itertools import count, product, dropwhile
+from itertools import count, product, dropwhile, takewhile
 from more_itertools import side_effect, consume, random_product, first_true
 from collections import deque
 
@@ -16,6 +16,9 @@ def transduce(xf, consumer, signal):
 def repeatedly(f):
     while True:
         yield f()
+
+def subtract(u, v):
+    return v[0] - u[0], v[1] - u[1]
 
 UP, DOWN, LEFT, RIGHT = (
     curses.KEY_UP,
@@ -44,20 +47,25 @@ Game = namedtuple(
         "apples",
         "apple",
         "alive",
+        "input",
     ],
 )
-
-
 def initgame(W, H):
+    global SCORE
+    SCORE = 0
+
     snakeXY = W // 2, H // 2
     snake = deque([snakeXY])
     snakeDirection = (0, 0)
     apples = repeatedly(lambda: random_product(range(W), range(H)))
     apple = next(apples)
     alive = True
+    input = None
     return Game(
             snake, snakeDirection, W, H, apples, apple,
-            alive,)
+            alive,
+            input,
+            )
 
 
 def translate(u, v, wall, ceiling): # vector translation on a 2D torus
@@ -85,25 +93,32 @@ def step(G):
 
 def play(G, INPUT):  # could dispatch with a dict?
     """Takes game state and INPUT, returns updated game state"""
+    G = G._replace(input=INPUT)
     G = turn(G, INPUT)
     G = step(G)
-    if not G.alive:
-        return None
-    elif G.snake[-1] == G.apple:
+    if G.snake[-1] == G.apple:
+        global SCORE
+        SCORE += 1
         return G._replace(
-                apple=first_true(G.apples, lambda a: a not in set(G.snake)))
+                apple=first_true(G.apples, lambda a: a not in G.snake))
     else:
         G.snake.popleft()
         return G
+
+APPLE_SYMBOL = "@"
+SNAKE_SYMBOL = "&"
 
 
 def show(G):  # do I pass curses-specific attributes here?
     # thinking of punting and just adding a "META" field
     """Takes a game state and returns a ready-to-render list of tuples (x, y, symbol)"""
-    return (
-        (*G.apple, "@"),
-        (0, 0, ascii(G)),
-    ) + tuple((*_, "&") for _ in G.snake)
+    center = G.snake[-1]
+    ret = (
+        (G.apple,APPLE_SYMBOL),
+        # (0, 0, ascii(G)),
+        # (1,1, ascii(G.input)),
+    ) + tuple((_,SNAKE_SYMBOL) for _ in G.snake)
+    return ((*translate(subtract(center, u), (G.W // 2, G.H // 2), G.W, G.H), sym) for u, sym in ret)
 
 
 INPUT_TIMEOUT_MS = 70
@@ -116,24 +131,40 @@ def initscr(scr):
         for i in range(0, curses.COLORS):
             curses.init_pair(i + 1, i, -1)
         scr.timeout(INPUT_TIMEOUT_MS)
+        scr.addstr(10, 10, "Badger's Snake ⾘的⺒", curses.A_BOLD)
+
+    return handler
+
+def bye(scr):
+    def handler():
+        curses.flash()
+        global SCORE
+        scr.addstr(10, 10, "Thanks for playing! Score: %d" % (SCORE))
+        scr.addstr(12, 12, "Press q to quit the game.")
+        consume(takewhile(lambda i: i != ord("q"), repeatedly(lambda:scr.getch())))
 
     return handler
 
 
 def draw(scr):
+    ATTRIBUTES = {
+        APPLE_SYMBOL: curses.A_BOLD | curses.color_pair(197),
+        SNAKE_SYMBOL: curses.A_BOLD | curses.color_pair(47),
+            }
     def handler(serialized):
         scr.clear()
         for x, y, symbol in serialized:
-            scr.addstr(y, x, symbol)
+            scr.addstr(y, x, symbol, ATTRIBUTES[symbol])
 
     return handler
 
 
 def main(scr):
     H, W = scr.getmaxyx()
+    G = initgame(W - 1, H - 1)
     inputs = dropwhile(lambda i: i not in ARROWS, repeatedly(lambda: scr.getch()))
-    states = transduce(play, initgame(W - 1, H - 1), inputs)
-    consume(side_effect(draw(scr), map(show, states), before=initscr(scr)))
-
+    states = takewhile(lambda g: g.alive, transduce(play, G, inputs))
+    run = side_effect(draw(scr), map(show, states), before=initscr(scr), after=bye(scr))
+    consume(run)
 
 core = curses.wrapper(main)
